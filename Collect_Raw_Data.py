290adf3c0b4b3d2fd9568a75f3c110fd1ac3c10b
SadrikA78 <31371511+SadrikA78@users.noreply.github.com>
2017-08-26 20:49:57 +0300

    Collect_Raw_Data.py
    Collect raw data into vk.com API

---
+++ Collect_Raw_Data.py
@@ -0,0 +1,32 @@
+# coding: utf8
+import calendar
+import time
+import datetime
+import httplib
+import urllib
+import json
+import ast
+import csv
+import sys
+import codecs
+import sched, time
+import threading
+
+latitude1= ""
+longitude1=""
+radius1=""
+
+
+def get_vk(latitude,longitude,radius):
+
+
+    get_request =  '/method/places.getCheckins?latitude=' + latitude
+    get_request+= '&longitude=' + longitude
+    get_request+= '&offset=3&count=100&friends_only=0&need_places=0&v=5.68'
+    local_connect = httplib.HTTPSConnection('api.vk.com', 443)
+    local_connect.request('GET', get_request)
+    a=local_connect.getresponse().read()
+    return a
+
+cc=get_vk(latitude1,longitude1,radius1)
+p = ast.literal_eval(cc)
