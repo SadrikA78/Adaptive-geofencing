e577387afa674a90d8d42088217b089edb4375a6
SadrikA78 <31371511+SadrikA78@users.noreply.github.com>
2017-08-26 23:16:22 +0300

    Update Collect_Raw_Data.py

--- Collect_Raw_Data.py
+++ Collect_Raw_Data.py
@@ -15,7 +15,7 @@ import threading
 latitude1= ""
 longitude1=""
 radius1=""
-
+access_token=""
 
 def get_vk(latitude,longitude,radius):
 
@@ -23,6 +23,7 @@ def get_vk(latitude,longitude,radius):
     get_request =  '/method/places.getCheckins?latitude=' + latitude
     get_request+= '&longitude=' + longitude
     get_request+= '&offset=3&count=100&friends_only=0&need_places=0&v=5.68'
+    et_request+= '&access_token='+ access_token
     local_connect = httplib.HTTPSConnection('api.vk.com', 443)
     local_connect.request('GET', get_request)
     a=local_connect.getresponse().read()
